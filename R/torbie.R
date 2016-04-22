d5bb5af0f91a3a0bf66f1f80b196c3e71df29b86
Adnan Fiaz <afiaz@mango-solutions.com>
2016-04-22 14:57:17 +0100

    Some more documentation

--- R/torbie.R
+++ R/torbie.R
@@ -3,7 +3,7 @@
 #' The torbie package provides the function \code{\link{simulate_values}} which allows you to simulate
 #' values from either the Normal, Poisson or Binomial distribution.
 #'
-#'
+#' Some more documentation
 #' @docType package
 #' @name torbie
 NULL
